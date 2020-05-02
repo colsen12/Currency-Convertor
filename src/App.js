f652f084b5d1db4c57c4affe21e018015281b019
github_colsen12 <colsen788@gmail.com>
2020-05-01 22:33:23 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import CurrencyRow from "./components/CurrencyRow"
 import './App.css';
 
+const API = "https://api.exchangeratesapi.io/latest"
+
 function App() {
+
+const [currencyOptions, setCurrencyOptions] = useState([])
+const [fromCurrency, setFromCurrency] = useState()
+const [toCurrency, setToCurrency] = useState()
+const [exchangeRate, setExchangeRate] = useState()
+const [amount, setAmount] = useState(1)
+const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+let toAmount, fromAmount
+if (amountInFromCurrency) {
+  fromAmount = amount
+  toAmount = amount * exchangeRate
+} else {
+  toAmount = amount
+  fromAmount = amount / exchangeRate
+}
+
+useEffect(() => {
+  fetch(API)
+  .then(res => res.json())
+  .then(data => {
+    const firstCurrency = Object.keys(data.rates)[0]
+    setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+    setFromCurrency(data.base)
+    setToCurrency(firstCurrency)
+    setExchangeRate(data.rates[firstCurrency])
+  })
+}, [])
+
+useEffect(() => {
+  if (fromCurrency != null && toCurrency != null)
+{  fetch(`${API}?base=${fromCurrency}&symbols=${toCurrency}`)
+  .then(res => res.json())
+  .then(data => setExchangeRate(data.rates[toCurrency]))
+}
+}, [fromCurrency, toCurrency])
+
+const handleFromChangeAmount = (e) => {
+  setAmount(e.target.value)
+  setAmountInFromCurrency(true)
+}
+
+const handleToChangeAmount = (e) => {
+  setAmount(e.target.value)
+  setAmountInFromCurrency(false)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Convert</h1>
+      <CurrencyRow 
+      currencyOptions={currencyOptions}
+      selectedCurrency={fromCurrency}
+      onChangeCurrency={e => setFromCurrency(e.target.value)}
+      amount={fromAmount}
+      onChangeAmount={handleFromChangeAmount}/>
+      <div className="equals">=</div>
+      <CurrencyRow 
+      currencyOptions={currencyOptions}
+      selectedCurrency={toCurrency}
+      onChangeCurrency={e => setToCurrency(e.target.value)}
+      amount={toAmount}
+      onChangeAmount={handleToChangeAmount}/>
     </div>
   );
 }
